4654705561d8a0058e8d50689501da6048feafea
JohnsonNyamweya <johnsonomari1032@gmail.com>
2022-02-21 13:08:55 +0300

    first commit

---
+++ app/src/main/java/com/johnsonnyamweya/torch/MainActivity.java
@@ -0,0 +1,61 @@
+package com.johnsonnyamweya.torch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnTorch;
+    Boolean isTorchOn = false;
+    CameraManager cameraManager;
+    String cameraId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        boolean isTorchAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        btnTorch = findViewById(R.id.btn_torch);
+
+        if (!isTorchAvailable){
+            btnTorch.setEnabled(false);
+            btnTorch.setText("Torch is not available");
+        }
+        try {
+
+            cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            cameraId = cameraManager.getCameraIdList()[0];
+        }catch (Exception e){ }
+        btnTorch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTorch(isTorchOn);
+
+                if (isTorchOn) {
+                    //Turn off the torch
+                    isTorchOn = false;
+                    btnTorch.setText("TURN ON TORCH");
+                }else {
+                    //Turn on the torch
+                    isTorchOn = true;
+                    btnTorch.setText("TURN OFF TORCH");
+                }
+
+                        }
+
+        });
+    }
+
+    public void switchTorch(boolean status){
+        try {
+            cameraManager.setTorchMode(cameraId, status);
+        }catch (Exception e){}
+    }
+}
